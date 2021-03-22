1236563fff6c0ffca8051eba8ec966b06ec57e19
AbhimanyuGupta20 <adgupta2@wisc.edu>
2021-03-23 02:16:41 +0800

    Updated data reader interface.

--- QuestionDataReaderInterface.java
+++ QuestionDataReaderInterface.java
@@ -3,13 +3,6 @@ import java.io.IOException;
 import java.util.List;
 import java.io.FileReader;
 
-
 public interface QuestionDataReaderInterface {
 	List<Question> read(FileReader reader) throws IOException;
 }
-
-//public interface QuestionDataReaderInterface {
-//	List<Question> read(FileReader reader) throws IOException;
-//	int remove(Question question);
-//	int add(Question question);
-//}
